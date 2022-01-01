fa56dc18d91ddc29b140834f6abfc241dc167b4c
lucasquearis <lucasquearis@hotmail.com>
2022-01-01 16:18:55 -0300

    exercicios operadores

---
+++ src/ExerciciosOperadores.java
@@ -0,0 +1,11 @@
+public class ExerciciosOperadores {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+        int soma = numero1 + numero2;
+        System.out.println("a soma é "+ soma);
+        System.out.println("a multiplicação é "+ (numero1 * numero2));
+        System.out.println("a divisão é "+ (numero1 / numero2));
+        System.out.println("a subtração é "+ (numero1 - numero2));
+    }
+}
