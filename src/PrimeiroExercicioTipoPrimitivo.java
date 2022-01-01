88b09ac9014629fbe768417e85fbbea68d3d0add
lucasquearis <lucasquearis@hotmail.com>
2022-01-01 12:57:02 -0300

    feito desafio tipos primitivos

---
+++ src/PrimeiroExercicioTipoPrimitivo.java
@@ -0,0 +1,14 @@
+public class PrimeiroExercicioTipoPrimitivo {
+    public static void main(String[] args) {
+        String nomePessoa = "Lucas";
+        String enderecoPessoa = "Santa Felicidade";
+        int telefonePessoa = 999999999;
+        double salario = 1000;
+        char sexo = 'M';
+        int idade = 25;
+        String estadoCivil = "casado";
+
+        System.out.println("O " + nomePessoa + " domiciliado no endereço " + enderecoPessoa + " e telefone " + telefonePessoa + " nao possui nenhum tipo de pendencia");
+        System.out.println("O trabalhador " + nomePessoa + " do sexo " + sexo + ", idade " + idade + ", estado civil "+estadoCivil+" e salário "+salario+" encontra-se empregado nesse estabelecimento");
+    }
+}
